90e9981f68f9ee3c4de00645b393ebd0b858aa41
Manny Costa <emanweb@gmail.com>
2026-04-12 23:50:44 -0700

    new feature Weekly Administration Updates

--- front-page.php
+++ front-page.php
@@ -233,6 +233,69 @@ get_header();
         </div>
       </section>
 	<?php endwhile; endif; ?>
+
+	<?php if( have_rows('weekly_administration_updates')): while( have_rows('weekly_administration_updates') ) : the_row(); ?>
+      <section class="admin-updates">
+        <div class="container admin-updates__container">
+          <h2 class="h2 js-type"><?php echo get_sub_field('title'); ?></h2>
+          <div class="admin-updates__description" data-aos="fade-in" data-aos-duration="2000"><?php echo get_sub_field('description'); ?></div>
+			<?php
+			$args = array(  
+				'post_type' => 'weekly-administration-updates',
+				'post_status' => 'publish',
+				'posts_per_page' => 4,
+				'ignore_sticky_posts' => false,
+				'orderby' => 'date', 
+        'order' => 'DESC', 
+			);  
+			
+			if(!empty(get_sub_field('case_studies_list'))){
+				$args['post__in'] = get_sub_field('case_studies_list');
+				$args['orderby'] = 'post__in';
+			}
+			
+			$loop = new WP_Query( $args ); 
+			$string = '';
+      $case_link_mode = check_using_ip_address();
+			if($loop->have_posts() ): ?>
+			<div class="admin-updates__list">
+			<?php
+			$latest_article = 1; 
+			$totalpost = $loop->found_posts;
+			while ( $loop->have_posts() ) : $loop->the_post(); 
+			global $post; ?>
+      <a class="admin-updates__list-item <?php if($case_link_mode =='popup') { ?>popup-link<?php } ?>" data-caseurl="<?php echo esc_url(get_the_permalink()); ?>" href="<?php if($case_link_mode =='popup') { echo "#";  } else { echo esc_url(get_the_permalink()); } ?>" <?php if($case_link_mode =='popup') { ?>data-popup="#popupSolution12"<?php } ?>>
+              <div class="admin-updates__list-image">
+                <picture>
+                <?php 
+				if(get_post_thumbnail_id( get_the_ID())){ ?> 
+          <img src="<?php echo esc_url(get_the_post_thumbnail_url(get_the_ID(), 'full')); ?>" alt="<?php echo esc_attr(wp_strip_all_tags(get_the_title())); ?>"/>
+				<?php } ?>
+                </picture>
+              </div>
+              <div class="admin-updates__list-content">
+        <div class="admin-updates__list-text"><?php echo wp_kses_post(get_the_title()); ?></div>
+        <div class="admin-updates__list-company"><?php echo esc_html(get_field('client_name')); ?></div>
+              </div></a>
+			<?php endwhile; ?>  
+			</div>
+			<?php endif; 
+      wp_reset_postdata();
+			?>
+			<?php if(!empty(get_sub_field('link'))) { ?>
+			<div class="admin-updates__list-more" data-aos="fade-up" data-aos-duration="2000"><a class="btn-more" href="<?php echo get_sub_field('link')['url']; ?>">
+              <div class="btn-more__text">Explore<br> More</div>
+              <div class="btn-more__icon">
+                <svg class="icon" width="24" height="24" viewBox="0 0 24 24">
+                  <use xlink:href="<?php echo get_template_directory_uri(); ?>/images/sprites/main.stack.svg#image-arrow-top-right"></use>
+                </svg>
+              </div></a>
+			</div>
+			<?php } ?>
+        </div>
+      </section>
+	<?php endwhile; endif; ?>
+
     </main>
 	<?php endwhile; // End of the loop. ?>
 <?php
